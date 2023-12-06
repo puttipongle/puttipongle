229d82f9e0beeb6ecddc51639f8d1cdf3c416c23
Puttipong Leekeawsai <152689693+puttipongle@users.noreply.github.com>
2023-12-06 13:04:40 +0700

    Delete Lab00 directory

--- Lab00/Lab00/Lab00_4_4.cpp
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-main(){
-	printf("study every time\n");
-	printf("Review lesson\n");
-	printf("Complete the job\n");
-	printf("Intend to take the exam\n");
-}
--- Lab00/Lab00/Lab00_4_5.cpp
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-main(){
-	printf("Fundraising\n");
-	printf("Find an online store\n");
-	printf("Invest in products\n");
-	printf("Sell products\n");
-	printf("Promote on social\n");
-}
